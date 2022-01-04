3202152986ad14376c7cf60caf4836da66b54728
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-01-04 12:39:09 +0100

    ngclass and ngstyle

--- src/app/direct/direct.component.ts
+++ src/app/direct/direct.component.ts
@@ -8,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class DirectComponent implements OnInit {
   bgColor = 'pink';
   cl = 'black';
+
+  class1 = true;
+  class2 = false;
+  class3 = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  switchClass() {
+    if (this.class1) {
+      this.class1 = false;
+      this.class2 = true;
+    } else if (this.class2) {
+      this.class2 = false;
+      this.class3 = true;
+    } else {
+      this.class3 = false;
+      this.class1 = true;
+    }
+  }
 }
